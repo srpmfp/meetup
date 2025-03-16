1f56c45d00f8b2108f987eb6470ba652e07340e4
srpmfp <sean@mothnflame.com>
2025-03-16 15:20:42 -0400

    updated useEffect toggle

--- src/components/CitySearchView/CitySearch.jsx
+++ src/components/CitySearchView/CitySearch.jsx
@@ -1,35 +1,80 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
-const CitySearch = ({ allLocations }) => {
-    const [query, setQuery] = useState('');
-    const [suggestion, setSuggestion] = useState([]);
-    const [showSuggestion, setShowSuggestion] = useState(false);
-    const handleInput = (event) => {
-        const value = event.target.value;
-        const filter = allLocations ? allLocations.filter((location) => {
-            return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
-        }) : [];
-        setQuery(value);
-        setSuggestion(filter);
+const CitySearch = ({ allLocations, setCurrentCity, setReducedLocations }) => {
 
-    }
-    return (
-        <div id='city-search'>
-            <input
-                type='text'
-                className='city'
-                placeholder='name of city'
-                value={query}
-                onFocus={() => setShowSuggestion(true)}
-                onChange={handleInput}
-            />
+  const [query, setQuery] = useState('');
+  const [suggestions, setSuggestions] = useState([]);
+  const [showSuggestions, setShowSuggestions] = useState(false);
+  const [toggle, setToggle] = useState(false);
 
-            {showSuggestion ? <ul className='suggestions'> {suggestion.map((suggestion) => {
-                return <li key={suggestion}>{suggestion}</li>
-            })} <li key='see all cities'> <b>See All Cities</b></li> </ul> : null}
+  useEffect(() => {
+    setSuggestions(allLocations);
+    // setToggle(toggle);
+  }, [`${allLocations}`, setCurrentCity, setReducedLocations]);
 
-        </div>
-    )
+  //filter locations based on user input
+  const handleInputChanged = (event) => {
+    const value = event.target.value;
+
+    const filteredLocations =
+      allLocations ? allLocations.filter((location) => {
+        return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
+      }) : [];
+    setQuery(value);
+    setSuggestions(filteredLocations);
+
+  };
+
+  const handleItemClicked = (event) => {
+    const value = event.target.textContent;
+
+
+    setQuery(value);
+    setShowSuggestions(false);
+    setCurrentCity(value);
+  };
+
+  const queryClear = () => {
+    setCurrentCity('See All Cities')
+    setQuery('')
+  }
+
+  return (
+    <div id="city-search">
+      <input
+        type="text"
+        className="city"
+        placeholder="Search for a city"
+        value={query}
+        onFocus={() => setShowSuggestions(true)}
+        onChange={handleInputChanged}
+      />
+      {showSuggestions ?
+        <ul className="suggestions">
+          {query === '' ? null :
+
+            <div className="query">
+
+            {query}
+              <button onClick={queryClear} >X</button>
+            </div>
+
+          }
+          {
+            suggestions.map((suggestion) => {
+              return <li role='event' onClick={handleItemClicked} key={suggestion}>{suggestion}</li>
+            })
+          }
+          <li key='See all cities' onClick={() => { handleItemClicked, setReducedLocations(toggle); setToggle(!toggle) }}>
+            {!toggle ? <b role="toggle">See All Cities</b> : <b role="toggle">Show Less</b>}
+          </li>
+        </ul>
+        : null
+      }
+    </div>
+  )
 }
+
+
 
 export default CitySearch;
