c4fe43c53ced356e12f88da5024ddfb242809ac2
srpmfp <sean@mothnflame.com>
2025-03-25 22:50:32 -0400

    atatus config added

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,9 @@ import './index.css'
 import App from './App.jsx'
 
 
+import atatus from 'atatus-spa';
+atatus.config('2f07950c97874463a33e94d16c0a0939').install();
+
 
 
 createRoot(document.getElementById('root')).render(
