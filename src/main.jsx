81890002cc5a74ce51d48c7bebd8f214775e8de2
srpmfp <sean@mothnflame.com>
2025-03-30 23:09:26 -0400

    main.jsx updated with service worker

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration.js';
 
 
 import atatus from 'atatus-spa';
