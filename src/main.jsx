ce572b754574fb530070d6022a86e478f0e273e0
srpmfp <sean@mothnflame.com>
2025-03-31 22:32:05 -0400

    added serverice worker * as

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import serviceWorkerRegistration from './serviceWorkerRegistration.js';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration.js';
 
 
 import atatus from 'atatus-spa';
