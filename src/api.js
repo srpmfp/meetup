f82084eb23d9fffa61674a0ed3dd5e5473ca98dd
srpmfp <160367191+srpmfp@users.noreply.github.com>
2025-04-01 20:49:02 -0400

    rm NProgress

--- src/api.js
+++ src/api.js
@@ -26,7 +26,6 @@ export const getEvents = async () => {
         const response = await fetch(url);
         const result = await response.json();
         if (result) {
-         
             localStorage.setItem("lastEvents", JSON.stringify(result.events));
             return result.events;
         } else return null;
