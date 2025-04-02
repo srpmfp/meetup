06eab875daf7dae6242105790ae9e5e1416a8d52
srpmfp <160367191+srpmfp@users.noreply.github.com>
2025-04-01 23:02:46 -0400

    hook name updated with set

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ const App = () => {
 
   useEffect(() => {
     if(navigator.online){
-      warningAlert('')
+      setWarningAlert('')
     }else {
       setWarningAlert('You are offline. Some features may not work as expected.');
     }
