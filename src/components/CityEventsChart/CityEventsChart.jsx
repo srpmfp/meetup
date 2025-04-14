403ac6d5cdc4e4ca6206792b481dc39d0472fde5
srpmfp <sean@mothnflame.com>
2025-04-14 15:27:38 -0400

    corrected chart filtering

--- src/components/CityEventsChart/CityEventsChart.jsx
+++ src/components/CityEventsChart/CityEventsChart.jsx
@@ -2,45 +2,51 @@ import React, { useState, useEffect, PureComponent } from 'react';
 import PropTypes from 'prop-types';
 
 import {
-
     Scatter,
     ScatterChart,
-    XAxis, YAxis,
+    XAxis,
+    YAxis,
     CartesianGrid,
     Tooltip,
-    ResponsiveContainer
+    ResponsiveContainer,
 } from 'recharts';
-
 
 const CityEventsChart = ({ allLocations, events }) => {
     const [data, setData] = useState([]);
 
     useEffect(() => {
         setTimeout(() => {
-            setData(getData())
+            setData(getData());
         }, 1500);
-    },
-        [`${events}`]);
+    }, [`${events}`]);
 
     const getData = () => {
         // counts the number of events in each city
         const data = allLocations.map((location) => {
             const count = events.filter((event) => event.location === location).length;
-            const city = location.split(', ')[0].substring(0, 3);
-
+            const city = location.split(', ')[0];
 
             return { count, city };
-        })
-        const filteredData = data.filter((location => location.count > 0));
-        return filteredData;
+        });
+        // const filteredData = data.filter((location) => location.count > 0);
+        return data;
     };
     class CustomizedAxisTick extends PureComponent {
+
         render() {
             const { x, y, payload } = this.props;
 
             return (
                 <g transform={`translate(${x},${y})`}>
-                    <text x={0} y={0} dy={-4} textAnchor="end" fontSize={16} padding="no-gap" fill="#ffffff" transform="rotate(-90)">
+                    <text
+                        x={-10}
+                        y={0}
+                        dy={16}
+                        textAnchor='end'
+                        fontSize={10}
+                        padding='no-gap'
+                        fill='#ffffff'
+                        transform='rotate(-35)'>
                         {payload.value}
                     </text>
                 </g>
@@ -49,30 +55,42 @@ const CityEventsChart = ({ allLocations, events }) => {
     }
 
     return (
-
-
-        <ResponsiveContainer width="99%" height={350}>
-
+        <ResponsiveContainer
+            width='99%'
+            height={300}>
             <ScatterChart
-                width={1000}
-                height={350}
+
                 margin={{
                     top: 20,
-                    right: 50,
-                    bottom: 50,
-
+                    right: 10,
+                    bottom: 20,
                 }}>
                 <CartesianGrid />
-                <XAxis type="category" dataKey="city" orientation="bottom" padding="no-gap" interval="equidistantPreserveStartEnd" tick={<CustomizedAxisTick />} name="City" />
-                <YAxis type="number" dataKey="count" name="Number of Events" tick={<CustomizedAxisTick />} />
+                <XAxis
+                    type='category'
+                    dataKey='city'
+                    orientation='bottom'
+                    padding='no-gap'
+                    interval='PreserveStartEnd'
+                    tick={<CustomizedAxisTick />}
+                    name='City'
+                />
+                <YAxis
+                    type='number'
+                    dataKey='count'
+                    name='Number of Events'
+                    tick={<CustomizedAxisTick />}
+                />
                 <Tooltip cursor={{ strokeDasharray: '3 3' }} />
-                <Scatter name="Number of Events" data={data} fill="#ffffff" />
+                <Scatter
+                    name='Number of Events'
+                    data={data}
+                    fill='#ffffff'
+                />
             </ScatterChart>
         </ResponsiveContainer>
-
-
-    )
-}
+    );
+};
 
 CityEventsChart.propTypes = {
     allLocations: PropTypes.array.isRequired,
