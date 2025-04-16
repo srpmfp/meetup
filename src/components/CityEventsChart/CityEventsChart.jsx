3fa79f40711b5df8184badc89248800161360f80
srpmfp <sean@mothnflame.com>
2025-04-16 10:07:17 -0400

    updated yaxis with no decimals

--- src/components/CityEventsChart/CityEventsChart.jsx
+++ src/components/CityEventsChart/CityEventsChart.jsx
@@ -79,6 +79,7 @@ const CityEventsChart = ({ allLocations, events, currentNOE }) => {
                 <YAxis
                     type='number'
                     dataKey='count'
+                    allowDecimals={false}
                     name='Number of Events'
                     tick={<CustomizedAxisTick />}
                 />
